025daf27b77c937f38fe7137ea892f1659454f93
N Srinivas <striker.dbz@hotmail.com>
2014-12-28 23:52:41 +0530

    add credits and license

--- main.c
+++ main.c
@@ -1,6 +1,35 @@
 //in this project build options linker settings have been modified
 //to link to libusb.a
 
+/* Program Author : N.Srinivas a.k.a. NeutroN StrikeR
+    Date: 15-10-2014
+    email id: striker.dbz[at]hotmail.com 
+    
+    The code is provided with: The MIT License (MIT)
+
+Copyright (c) <year> <copyright holders>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+
+*/
+
 #include"libusb\include\lusb0_usb.h"
 #include <stdio.h>
 #include <stdlib.h>
